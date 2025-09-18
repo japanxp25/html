e49d389d7e1780d9505055d4f7557ef3217c6e28
JAPAN XP <japanxp25@gmail.com>
2025-09-18 15:01:30 +1000

    Create langText.js

---
+++ langText.js
@@ -0,0 +1,38 @@
+{
+	"en": [
+            { 
+				"textScore": "Score", 
+				"emptyList": "Reset", 
+				"textDownloadPng": "Download", 
+				"clearHistory": "Clear", 
+				"textDataSource": "Type",
+				"textInstgramId": "IG",
+				"textInstgramSave": "Save",
+				"textNoData": "No Data"
+			}
+    ],
+    "tc": [
+            { 
+				"textScore": "分數", 
+				"emptyList": "重設", 
+				"textDownloadPng": "下載", 
+				"clearHistory": "清空", 
+				"textDataSource": "類型",
+				"textInstgramId": "IG",
+				"textInstgramSave": "儲存",
+				"textNoData": "未有資料"
+			}
+    ],
+	"jp": [
+            { 
+				"textScore": "分數", 
+				"emptyList": "重設", 
+				"textDownloadPng": "下載", 
+				"clearHistory": "清空", 
+				"textDataSource": "類型",
+				"textInstgramId": "IG",
+				"textInstgramSave": "儲存",
+				"textNoData": "未有資料"
+			}
+    ]
+}	
