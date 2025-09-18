c2f36e7ebb0f8725fee6e3d151c6a57e596f26e9
JAPAN XP <japanxp25@gmail.com>
2025-09-18 15:05:54 +1000

    Create prefectureTempleData_tc.js

---
+++ prefectureTempleData_tc.js
@@ -0,0 +1,148 @@
+{
+    "北海道": [
+        { "id": "hkd_jingushrine", "name": "北海道神宮 (Hokkaido Jingu Shrine)", "checked": false },
+        { "id": "hkd_zenkoji", "name": "善光寺札幌別院 (Zenko-ji Temple Sapporo Branch)", "checked": false }
+    ],
+    "青森": [
+        { "id": "aom_osorezan", "name": "恐山菩提寺 (Osorezan Bodai-ji Temple)", "checked": false },
+        { "id": "aom_seiryuji", "name": "青龍寺 (Seiryū-ji Temple)", "checked": false }
+    ],
+    "岩手": [
+        { "id": "iwa_chusonji", "name": "中尊寺 (Chuson-ji Temple)", "checked": false },
+        { "id": "iwa_motsuji", "name": "毛越寺 (Motsu-ji Temple)", "checked": false },
+        { "id": "iwa_zoganji", "name": "蔵厳寺 (Zōgan-ji Temple)", "checked": false }
+    ],
+    "宮城": [
+        { "id": "miy_zuiganji", "name": "瑞巌寺 (Zuigan-ji Temple)", "checked": false },
+        { "id": "miy_entsuin", "name": "円通院 (Entsuin Temple)", "checked": false }
+    ],
+    "秋田": [
+        { "id": "aki_shizan", "name": "真山神社 (Shinzan Shrine)", "checked": false },
+        { "id": "aki_myohoji", "name": "妙法寺 (Myoho-ji Temple)", "checked": false }
+    ],
+    "山形": [
+        { "id": "yam_yamadera", "name": "山寺 (Yamadera Temple)", "checked": false },
+        { "id": "yam_hagurosan", "name": "羽黒山 (Mount Haguro)", "checked": false }
+    ],
+    "福島": [
+        { "id": "fuk_sazaedo", "name": "旧正宗寺三匝堂 (Sazaedo Temple)", "checked": false },
+        { "id": "fuk_enjo-ji", "name": "円蔵寺 (Enzō-ji Temple)", "checked": false }
+    ],
+    "茨城": [
+        { "id": "iba_kasamainari", "name": "笠間稲荷神社 (Kasama Inari Shrine)", "checked": false },
+        { "id": "iba_shizumine", "name": "静神社 (Shizumine Shrine)", "checked": false }
+    ],
+    "栃木": [
+        { "id": "toc_nikko", "name": "日光東照宮 (Nikko Toshogu Shrine)", "checked": false },
+        { "id": "toc_nikkofutarasan", "name": "日光二荒山神社 (Nikko Futarasan Shrine)", "checked": false }
+    ],
+    "埼玉": [
+        { "id": "sai_chichibu", "name": "秩父神社 (Chichibu Shrine)", "checked": false },
+        { "id": "sai_heirinji", "name": "平林寺 (Heirin-ji Temple)", "checked": false }
+    ],
+    "千葉": [
+        { "id": "chi_naritasan", "name": "成田山新勝寺 (Naritasan Shinsho-ji Temple)", "checked": false },
+        { "id": "chi_nihonji", "name": "日本寺 (Nihon-ji Temple)", "checked": false }
+    ],
+    "東京": [
+        { "id": "tok_sensoji", "name": "浅草寺 (Senso-ji Temple)", "checked": false },
+        { "id": "tok_meijijingu", "name": "明治神宮 (Meiji Jingu Shrine)", "checked": false },
+        { "id": "tok_zojoji", "name": "増上寺 (Zojo-ji Temple)", "checked": false },
+        { "id": "tok_gotokuji", "name": "豪徳寺 (Gotoku-ji Temple)", "checked": false },
+        { "id": "tok_jindaiji", "name": "深大寺 (Jindai-ji Temple)", "checked": false }
+    ],
+    "神奈川": [
+        { "id": "kan_engakuji", "name": "円覚寺 (Engaku-ji Temple)", "checked": false },
+        { "id": "kan_hasedera", "name": "長谷寺 (Hase-dera Temple)", "checked": false },
+        { "id": "kan_kenchoji", "name": "建長寺 (Kencho-ji Temple)", "checked": false }
+    ],
+    "新潟": [
+        { "id": "nii_yahiko", "name": "彌彦神社 (Yahiko Shrine)", "checked": false },
+        { "id": "nii_honjo-ji", "name": "本成寺 (Honjō-ji Temple)", "checked": false }
+    ],
+    "富山": [
+        { "id": "toy_zuiryuji", "name": "瑞龍寺 (Zuiryu-ji Temple)", "checked": false },
+        { "id": "toy_kokutaiji", "name": "国泰寺 (Kokutai-ji Temple)", "checked": false }
+    ],
+    "石川": [
+        { "id": "ish_kanazawa", "name": "金沢城 (Kanazawa Castle)", "checked": false },
+        { "id": "ish_nanatsuo", "name": "七尾城 (Nanao Castle)", "checked": false }
+    ],
+    "福井": [
+        { "id": "fuk_eiheiji", "name": "永平寺 (Eihei-ji Temple)", "checked": false },
+        { "id": "fuk_myotsuji", "name": "明通寺 (Myotsu-ji Temple)", "checked": false }
+    ],
+    "長野": [
+        { "id": "nag_zenkoji", "name": "善光寺 (Zenko-ji Temple)", "checked": false },
+        { "id": "nag_tōmyō-ji", "name": "東明寺 (Tōmyō-ji Temple)", "checked": false }
+    ],
+    "岐阜": [
+        { "id": "gif_kegonji", "name": "華厳寺 (Kegon-ji Temple)", "checked": false },
+        { "id": "gif_shinnyoji", "name": "真如寺 (Shinnyo-ji Temple)", "checked": false },
+        { "id": "gif_gifucastle", "name": "岐阜城 (Gifu Castle)", "checked": false }
+    ],
+    "愛知": [
+        { "id": "aic_atsutajingu", "name": "熱田神宮 (Atsuta Jingu Shrine)", "checked": false },
+        { "id": "aic_osukannon", "name": "大須観音 (Osu Kannon Temple)", "checked": false },
+        { "id": "aic_inuyamacastle", "name": "犬山城 (Inuyama Castle)", "checked": false }
+    ],
+    "三重": [
+        { "id": "mie_isejingu", "name": "伊勢神宮 (Ise Grand Shrine)", "checked": false },
+        { "id": "mie_kongoshoji", "name": "金剛證寺 (Kongosho-ji Temple)", "checked": false }
+    ],
+    "京都": [
+        { "id": "kyo_kinkakuji", "name": "金閣寺 (Kinkaku-ji Temple)", "checked": false },
+        { "id": "kyo_kiyomizudera", "name": "清水寺 (Kiyomizu-dera Temple)", "checked": false },
+        { "id": "kyo_fushimiinari", "name": "伏見稲荷大社 (Fushimi Inari Taisha Shrine)", "checked": false },
+        { "id": "kyo_byodoin", "name": "平等院 (Byodo-in Temple)", "checked": false },
+        { "id": "kyo_ginkakuji", "name": "銀閣寺 (Ginkaku-ji Temple)", "checked": false },
+        { "id": "kyo_nanzenji", "name": "南禅寺 (Nanzen-ji Temple)", "checked": false },
+        { "id": "kyo_toji", "name": "東寺 (To-ji Temple)", "checked": false },
+        { "id": "kyo_sanjusangendo", "name": "三十三間堂 (Sanjusangen-do Temple)", "checked": false }
+    ],
+    "大阪": [
+        { "id": "osa_shitennoji", "name": "四天王寺 (Shitenno-ji Temple)", "checked": false },
+        { "id": "osa_katsuo-ji", "name": "勝尾寺 (Katsuō-ji Temple)", "checked": false }
+    ],
+    "奈良": [
+        { "id": "nar_todaiji", "name": "東大寺 (Todai-ji Temple)", "checked": false },
+        { "id": "nar_horyuji", "name": "法隆寺 (Horyu-ji Temple)", "checked": false },
+        { "id": "nar_kofukuji", "name": "興福寺 (Kofuku-ji Temple)", "checked": false }
+    ],
+    "和歌山": [
+        { "id": "wak_koyasan", "name": "高野山 (Mount Koya)", "checked": false },
+        { "id": "wak_kumanonachi", "name": "熊野那智大社 (Kumano Nachi Taisha Shrine)", "checked": false }
+    ],
+    "島根": [
+        { "id": "shi_izumotaisha", "name": "出雲大社 (Izumo Taisha Shrine)", "checked": false },
+        { "id": "shi_kiyomizudera", "name": "清水寺 (Kiyomizu-dera Temple)", "checked": false }
+    ],
+    "広島": [
+        { "id": "hir_itsukushima", "name": "厳島神社 (Itsukushima Shrine)", "checked": false },
+        { "id": "hir_daishoin", "name": "大聖院 (Daisho-in Temple)", "checked": false }
+    ],
+    "徳島": [
+        { "id": "tok_ryozenji", "name": "霊山寺 (Ryozen-ji Temple)", "checked": false },
+        { "id": "tok_gokurakuji", "name": "極楽寺 (Gokuraku-ji Temple)", "checked": false }
+    ],
+    "香川": [
+        { "id": "kag_zentsuji", "name": "善通寺 (Zentsu-ji Temple)", "checked": false },
+        { "id": "kag_konpira", "name": "金刀比羅宮 (Kotohira-gu Shrine)", "checked": false }
+    ],
+    "愛媛": [
+        { "id": "ehi_ishiteji", "name": "石手寺 (Ishite-ji Temple)", "checked": false },
+        { "id": "ehi_taihoji", "name": "大宝寺 (Taiho-ji Temple)", "checked": false }
+    ],
+    "高知": [
+        { "id": "koc_chikurinji", "name": "竹林寺 (Chikurin-ji Temple)", "checked": false },
+        { "id": "koc_kongochoji", "name": "金剛頂寺 (Kongocho-ji Temple)", "checked": false }
+    ],
+    "佐賀": [
+        { "id": "sag_yutokuinari", "name": "祐徳稲荷神社 (Yutoku Inari Shrine)", "checked": false },
+        { "id": "sag_korinji", "name": "興倫寺 (Korin-ji Temple)", "checked": false }
+    ],
+    "沖縄": [
+        { "id": "oki_naminoue", "name": "波上宮 (Naminoue Shrine)", "checked": false },
+        { "id": "oki_enkakuji", "name": "円覚寺 (Enkaku-ji Temple)", "checked": false }
+    ]
+}
