607d0553b57bade95b196f426a6a1f0cbdd03c95
JAPAN XP <japanxp25@gmail.com>
2025-09-18 15:01:56 +1000

    Create prefectureList.js

---
+++ prefectureList.js
@@ -0,0 +1,26 @@
+{
+	"SPOT": [
+            { "lang": "en", "name": "SPOT LIST", "api": "/prefectureSpotData_en.js", "title": "Japan Tourist Spot", "icon": ""},
+			{ "lang": "jp", "name": "SPOT LIST", "api": "/prefectureSpotData_jp.js", "title": "Japan Tourist Spot", "icon": ""},
+			{ "lang": "tc", "name": "景點", "api": "/prefectureSpotData_tc.js", "title": "全日本足跡(景點)", "icon": ""},
+			{ "lang": "sc", "name": "景點", "api": "/prefectureSpotData_sc.js", "title": "全日本足跡(景點)", "icon": ""}
+    ],
+    "TOWER": [
+            { "lang": "en", "name": "TOWER LIST", "api": "/prefectureTowerData_en.js", "title": "Japan 1 Spot", "icon": ""},
+			{ "lang": "jp", "name": "TOWER LIST", "api": "/prefectureTowerData_jp.js", "title": "Japan 2 Spot", "icon": ""},
+			{ "lang": "tc", "name": "觀光塔", "api": "/prefectureTowerData_tc.js", "title": "全日本足跡(觀光塔)", "icon": ""},
+			{ "lang": "sc", "name": "觀光塔", "api": "/prefectureTowerData_sc.js", "title": "全日本足跡(觀光塔)", "icon": ""}
+    ],
+	"PREF": [
+            { "lang": "en", "name": "TOWER LIST", "api": "/prefectureData_en.js", "title": "Japan 2 Spot", "icon": ""},
+			{ "lang": "jp", "name": "TOWER LIST", "api": "/prefectureData_jp.js", "title": "Japan 2 Spot", "icon": ""},
+			{ "lang": "tc", "name": "懸觀光點", "api": "/prefectureData_tc.js", "title": "全日本足跡(懸觀點)", "icon": ""},
+			{ "lang": "sc", "name": "懸觀光點", "api": "/prefectureData_sc.js", "title": "全日本足跡(懸觀點)", "icon": ""}
+    ],
+	"TEMPLE": [
+            { "lang": "en", "name": "TOWER LIST", "api": "/prefectureTempleData_en.js", "title": "Japan 3 Spot", "icon": ""},
+			{ "lang": "jp", "name": "TOWER LIST", "api": "/prefectureTempleData_jp.js", "title": "Japan 3 Spot", "icon": ""},
+			{ "lang": "tc", "name": "寺廟", "api": "/prefectureTempleData_tc.js", "title": "全日本足跡(寺廟)", "icon": ""},
+			{ "lang": "sc", "name": "寺廟", "api": "/prefectureTempleData_sc.js", "title": "全日本足跡(寺廟)", "icon": ""}
+    ]
+}	
