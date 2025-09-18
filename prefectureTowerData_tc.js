751404f81e3be7d0b68c5c48ae9bd6b51d2d8232
JAPAN XP <japanxp25@gmail.com>
2025-09-18 15:04:19 +1000

    Create prefectureTowerData_tc.js

---
+++ prefectureTowerData_tc.js
@@ -0,0 +1,27 @@
+{
+    "東京": [
+        { "id": "tok_skytree", "name": "Tokyo Skytree", "checked": false },
+        { "id": "tok_tower", "name": "Tokyo Tower", "checked": false }
+    ],
+    "大阪": [
+        { "id": "osa_tsutenkaku", "name": "Tsutenkaku Tower", "checked": false },
+        { "id": "osa_umedasky", "name": "Umeda Sky Building (Kuchu Teien Observatory)", "checked": false }
+    ],
+    "兵庫": [
+        { "id": "hyo_kobeport", "name": "Kobe Port Tower", "checked": false }
+    ],
+    "神奈川": [
+        { "id": "kan_yokohamamarine", "name": "Yokohama Marine Tower", "checked": false },
+        { "id": "kan_yokohamalandmark", "name": "Yokohama Landmark Tower", "checked": false }
+    ],
+    "福岡": [
+        { "id": "fuk_fukuoka", "name": "Fukuoka Tower", "checked": false }
+    ],
+    "京都": [
+        { "id": "kyo_kyoto", "name": "Kyoto Tower", "checked": false }
+    ],
+    "北海道": [
+        { "id": "hkd_sapporotv", "name": "Sapporo TV Tower", "checked": false },
+        { "id": "hkd_goryokaku", "name": "Goryokaku Tower", "checked": false }
+    ]
+}
